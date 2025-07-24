506793afd64f7166aabbc29ce8a43fdd9307ca07
John Van Gogh <80774239+jonuoha60@users.noreply.github.com>
2025-07-24 11:35:22 -0400

    Update cart.php

--- cart.php
+++ cart.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="utf-8">
     <title>E-COMMERCE STORE</title>
-    <link rel="stylesheet" href="crt.css">
+    <link rel="stylesheet" href="cart.css">
     
       <link rel="stylesheet" type="" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
